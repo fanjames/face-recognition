f06b2b418ad408aa16cb226d2c9a4545ccf492a9
fanjames <fanhuajames@gmail.com>
2018-12-03 12:57:44 +0800

    Initial commit

---
+++ face_camera.py
@@ -0,0 +1,54 @@
+import face_recognition
+import cv2
+import numpy as np
+
+from PIL import Image
+from utils import draw_bounding_box_on_image
+
+
+fanhua_image = face_recognition.load_image_file("images/fanhua.jpg")
+fanhua_face_encoding = face_recognition.face_encodings(fanhua_image)[0]
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    fanhua_face_encoding
+]
+known_face_names = [
+    "樊华"
+]
+
+def detect_faces(frame):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    image = Image.fromarray(frame)
+
+    resize_scale = 2.0
+    frame = cv2.resize(frame, (0, 0), fx=1/resize_scale, fy=1/resize_scale)
+
+    face_locations = face_recognition.face_locations(frame)
+    face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+    face_names = []
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+        name = "Unknown"
+
+        # If a match was found in known_face_encodings, just use the first one.
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_face_names[first_match_index]
+
+        face_names.append(name)
+
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        top *= resize_scale
+        right *= resize_scale
+        bottom *= resize_scale
+        left *= resize_scale
+
+        draw_bounding_box_on_image(image, top, left, bottom, right, font="simkai", 
+                display_str_list=[name], use_normalized_coordinates=False)
+
+    image = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
+    return image
---
+++ utils.py
@@ -0,0 +1,52 @@
+import numpy as np
+import PIL.Image as Image
+import PIL.ImageColor as ImageColor
+import PIL.ImageDraw as ImageDraw
+import PIL.ImageFont as ImageFont
+
+
+def draw_bounding_box_on_image(image, ymin, xmin, ymax, xmax, color='red', font='simhei',
+                               thickness=4, display_str_list=[],
+                               use_normalized_coordinates=False):
+    draw = ImageDraw.Draw(image)
+    im_width, im_height = image.size
+    if use_normalized_coordinates:
+        (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                      ymin * im_height, ymax * im_height)
+    else:
+        (left, right, top, bottom) = (xmin, xmax, ymin, ymax)
+
+    draw.line([(left, top), (left, bottom), (right, bottom),
+               (right, top), (left, top)], width=thickness, fill=color)
+
+    if font is not None:
+        try:
+            font = ImageFont.truetype(f'{font}.ttf', int(im_width/30))
+        except IOError:
+            font = ImageFont.load_default()
+
+    # If the total height of the display strings added to the top of the bounding
+    # box exceeds the top of the image, stack the strings below the bounding box
+    # instead of above.
+    display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
+    # Each display_str has a top and bottom margin of 0.05x.
+    total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
+
+    if top > total_display_str_height:
+        text_bottom = top
+    else:
+        text_bottom = bottom + total_display_str_height
+    # Reverse list and print from bottom to top.
+    for display_str in display_str_list[::-1]:
+        text_width, text_height = font.getsize(display_str)
+        margin = np.ceil(0.05 * text_height)
+        draw.rectangle(
+            [(left, text_bottom - text_height - 2 * margin), 
+             (left + text_width, text_bottom)], fill=color)
+        draw.text(
+            (left + margin, text_bottom - text_height - margin),
+            display_str, fill='white', font=font)
+
+        text_bottom -= text_height - 2 * margin
+
+
