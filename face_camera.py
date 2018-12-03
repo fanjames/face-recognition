94cf0c3b13de6c54fe7f20900236393c3f076292
fanjames <fanhuajames@gmail.com>
2018-12-03 13:32:33 +0800

    Update

--- face_camera.py
+++ face_camera.py
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 import face_recognition
 import cv2
 import numpy as np
@@ -14,7 +16,7 @@ known_face_encodings = [
     fanhua_face_encoding
 ]
 known_face_names = [
-    "樊华"
+    u"樊华"
 ]
 
 def detect_faces(frame):
